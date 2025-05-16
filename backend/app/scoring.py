c3b4c974660f478e83688849d14a59c8b608d047
Khushbu Chourasia <khushbuchourasia63@gmail.com>
2025-05-16 17:47:46 +0530

    Remove files now ignored by .gitignore

--- backend/app/scoring.py
+++
@@ -1,9 +0,0 @@
-# app/scoring.py
-from sentence_transformers import SentenceTransformer, util
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def get_similarity_score(resume_text, job_description):
-    embeddings = model.encode([resume_text, job_description])
-    similarity = util.cos_sim(embeddings[0], embeddings[1])
-    return float(similarity[0][0]) * 100
